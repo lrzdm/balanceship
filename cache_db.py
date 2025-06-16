39e6fff26e4fbbf05bcf5053643b4006631c71ce
lrzdm <lorenzodemeo@gmail.com>
2025-06-16 21:40:58 +0200

    Update cache_db.py

--- cache_db.py
+++ cache_db.py
@@ -17,12 +17,13 @@ Base = declarative_base()
 # Scegli il database in base all'ambiente
 if os.environ.get("STREAMLIT_CLOUD") == "1":
     DATABASE_URL = os.environ.get("DATABASE_URL")
+    print("DATABASE_URL:", DATABASE_URL)
     engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 else:
     os.makedirs("data", exist_ok=True)
     DATABASE_URL = "sqlite:///data/financials_db.db"
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-print("✅ Database in uso:", DATABASE_URL)
+
 Session = scoped_session(sessionmaker(bind=engine))
 
 # Tabelle
