ec3db2147715ca9b2357eadcb4f0f95e1c2d32c5
lrzdm <lorenzodemeo@gmail.com>
2025-06-16 15:37:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
+import streamlit as st
 
 url = "postgresql+psycopg2://postgres:ukI1XkSjJ9KZXw8p@db.ushtfkgezrisfwddarye.supabase.co:5432/postgres"
 engine = create_engine(url)
 
 try:
     with engine.connect() as conn:
-        print("✅ Connessione riuscita")
+        st.write("✅ Connessione riuscita")
 except Exception as e:
-    print("❌ Errore:", e)
+    st.write("❌ Errore:", e)
 
 
