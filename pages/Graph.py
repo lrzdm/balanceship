5eac2c74a40a362ef58c646a5dfe61b4f0abcc46
lrzdm <lorenzodemeo@gmail.com>
2025-06-18 21:49:24 +0200

    Update Graph.py

--- pages/Graph.py
+++ pages/Graph.py
@@ -97,7 +97,7 @@ def render_kpis():
 
     # 🔁 Carica dati da DB
     try:
-        df_all_kpis = load_kpis_for_symbol_year()
+        df_all_kpis = df_kpis
         descriptions_dict = df_all_kpis.drop_duplicates(subset='symbol').set_index('description')['symbol'].to_dict()
         descriptions_available = sorted(descriptions_dict.keys())
         years_available = sorted(df_all_kpis['year'].astype(str).unique())
