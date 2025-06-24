1659eb6921347c89309ad8f15bb4870f627c309a
lrzdm <lorenzodemeo@gmail.com>
2025-06-24 22:46:44 +0200

    Update Database.py

--- pages/Database.py
+++ pages/Database.py
@@ -121,6 +121,7 @@ st.markdown(f"<div class='currency-info'>{currency_messages.get(currency, 'Numbe
   #      data_list = get_or_fetch_data(symbol, selected_years, description, stock_exchange)
    #     financial_data.extend(data_list)
 
+use_cache == True
 financial_data = []
 
 for exchange in selected_exchanges:
