3ba15b2f737a1a920790159da6a4c94a9d5bdf4d
lrzdm <lorenzodemeo@gmail.com>
2025-07-12 19:10:33 +0200

    Update Who_we_are.py

--- pages/Who_we_are.py
+++ pages/Who_we_are.py
@@ -55,7 +55,9 @@ st.markdown(f"<div class='logo-container'>{logo_html}</div>", unsafe_allow_html=
 
 # --- Startup Info + About us Side by Side ---
 st.markdown(""" 
-  <div style="max-width: 1000px; width: 100%; margin: 0 auto; padding: 30px; box-sizing: border-box; text-align: left; background-color: #f9f9f9; border-radius: 16px; box-shadow: 0 4px 12px rgba(0,0,0,0.1); color: #000;">
+  <div style="display: flex; justify-content: center; gap: 30px; flex-wrap: wrap; align-items: stretch; min-height: 400px; margin: 40px 0;">
+  <div class='credo-box' style="max-width: 900px; width: 100%; margin: 0; height: 100%; box-sizing: border-box; display: flex; background-color: #f9f9f9; border-left: 6px solid #0173C4; padding: 30px; border-radius: 12px; box-shadow: 0 4px 12px rgba(0,0,0,0.1);">
+    <div style="width: 100%;">
     <h2 style="text-align:center;">🌟 Our Credo</h2>
 
     <p><strong>🧭</strong><br/>
