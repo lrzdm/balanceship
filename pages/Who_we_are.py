9e3ef75cc465ea196f088339497b86ab5014bac8
lrzdm <lorenzodemeo@gmail.com>
2025-07-12 19:14:52 +0200

    Update Who_we_are.py

--- pages/Who_we_are.py
+++ pages/Who_we_are.py
@@ -83,13 +83,13 @@ profiles = [
  Experienced in financial analysis, accounting, and managing financial risks to support strategic decision-making. Proficient in Power BI and \
 Python.","images/Lorenzo De Meo_01.jpg"),
   ("William Herbert Gazzo","Professional with a solid business background and a professional training from SDA Bocconi. \
-Specializing in project management and business planning. He boasts extensive experience in consulting firms \
+Specialized in project management and business planning. He boasts extensive experience in consulting firms \
 and multinational companies, where he has held managerial roles.","images/William H Gazzo_01.jpg"),
-  ("Gabriele Schininà","Professional with a solid financial background and training from SDA Bocconi. Specializing in financial modelling, \
+  ("Gabriele Schininà","Professional with a solid financial background and training from SDA Bocconi. Specialized in financial modelling, \
   strategic planning, and budget management. He boasts extensive experience in listed and non-listed multinational companies, with roles in \
 business controlling.","images/Gabriele Schinina_01.jpg"),
   ("Giovanni Serusi","Professional with a solid scientific background, specializing in clinical and cognitive neuroscience and with economic training\
-from SDA Bocconi. Specializing in competitive intelligence and scouting new investment opportunities with a focus on the life-science sector.","images/Giovanni Serusi_01.jpg"),
+from SDA Bocconi. Specialized in competitive intelligence and scouting new investment opportunities with a focus on the life-science sector.","images/Giovanni Serusi_01.jpg"),
 ]
 cards = ""
 for name, desc, img in profiles:
