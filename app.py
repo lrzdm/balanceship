9f1d99616ad7e08375081073105f74931302ee2f
lrzdm <lorenzodemeo@gmail.com>
2025-07-17 19:19:24 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    verification_tag = '<meta name="google-site-verification" content="P8TBKoEhxpfTVVZF7CTXHNp9dQVC0ynMTo18I9xdzvo" />'
+    return f'''
+    <html>
+    <head>
+        {verification_tag}
+        <title>Sitemap service for balanceship.net</title>
+    </head>
+    <body>
+        <h1>Sitemap service for balanceship.net</h1>
+        <p><a href="/sitemap.xml">View sitemap</a></p>
+    </body>
+    </html>
+    '''
+#@app.route('/')
+#def home():
+#    return "Sitemap service for balanceship.net"
+
+@app.route('/sitemap.xml')
+def sitemap():
+    sitemap_xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://balanceship.net/</loc>
+    <lastmod>2025-01-16</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://balanceship.net/Database</loc>
+    <lastmod>2025-01-16</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://balanceship.net/KPI_Dashboard</loc>
+    <lastmod>2025-01-16</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://balanceship.net/Graph</loc>
+    <lastmod>2025-01-16</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://balanceship.net/Who_we_are</loc>
+    <lastmod>2025-01-16</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+</urlset>'''
+    return Response(sitemap_xml, mimetype='application/xml')
+
+@app.route('/robots.txt')
+def robots():
+    robots_txt = '''User-agent: *
+Allow: /
+
+Sitemap: https://balanceship-sitemap.onrender.com/sitemap.xml'''
+    return Response(robots_txt, mimetype='text/plain')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
