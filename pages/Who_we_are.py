de5499c8082cd4725cd08ca1e3a01c5f867c321c
lrzdm <lorenzodemeo@gmail.com>
2025-10-23 19:23:34 +0200

    Update Who_we_are.py

--- pages/Who_we_are.py
+++ pages/Who_we_are.py
@@ -6,10 +6,6 @@ import requests
 import uuid
 
 MEASUREMENT_ID = "G-Q5FDX0L1H2" # Il tuo ID GA4 
-API_SECRET = "kRfQwfxDQ0aACcjkJNENPA" # Quello creato in GA4 
-
-if "client_id" not in st.session_state:
-    st.session_state["client_id"] = str(uuid.uuid4())
 
 # --------------- Client-side GA4 -----------------
 st.markdown(f"""
@@ -19,28 +15,13 @@ st.markdown(f"""
   window.dataLayer = window.dataLayer || [];
   function gtag(){{dataLayer.push(arguments);}}
   gtag('js', new Date());
-  gtag('config', '{MEASUREMENT_ID}');
+  gtag('config', '{MEASUREMENT_ID}', {{
+    page_path: window.location.pathname
+  }});
 </script>
 """, unsafe_allow_html=True)
 
-def send_pageview():
-    url = f"https://www.google-analytics.com/mp/collect?measurement_id={MEASUREMENT_ID}&api_secret={API_SECRET}"
-    payload = {
-        "client_id": st.session_state["client_id"],
-        "events": [
-            {
-                "name": "page_view",
-                "params": {
-                    "page_title": "Who_we_are",
-                    "page_location": "https://www.balanceship.net/Who_we_are",
-                    "engagement_time_msec": 1
-                }
-            }
-        ]
-    }
-    requests.post(url, json=payload)
 
-send_pageview()
 
 #Google tag:
 add_meta_tags(
@@ -342,5 +323,6 @@ st.markdown("""
     &copy; 2025 BalanceShip. All rights reserved.
 </div>
 """, unsafe_allow_html=True)
+
 
 
